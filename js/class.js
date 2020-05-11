6aaf2ceea1f5c8d814cb34703e7f7111132ec3fe
Walace Paz <walacepaz11@gmail.com>
2020-05-10 22:27:41 -0300

    tudo enviado

---
+++ js/class.js
@@ -0,0 +1,262 @@
+class Equacao{
+    a = 0
+    b = 0
+    c = 0
+    delta = undefined
+    x1 = undefined
+    x2 = undefined
+    x1Fracao = undefined 
+    x2Fracao =  undefined
+    x1Raiz = undefined
+    x2Raiz = undefined
+    Xv = undefined
+    Yv = undefined
+    constructor(a, b, c){
+        this.a = Number(a)
+        this.b = Number(b)
+        this.c = Number(c)
+        if(a < 0){
+            this.a *= -1
+            this.b *= -1 
+            this.c *= -1 
+        }
+        this.Delta()
+        this.x()
+        this.xv()
+        this.yv()
+        if(this.delta > 0 && this.x2 > this.x1){
+            let x = this.x1
+            this.x1 = this.x2
+            this.x2 = x
+        }
+    }
+    Delta(){
+        this.delta = this.b**2 - 4 * this.a * this.c
+    }
+    x(){
+        let a = this.a
+        let b = this.b
+        let delta = this.delta
+        let x1 = 0
+        let x2 = 0
+        if(delta >= 0){
+            x1 = (-b + Math.sqrt(delta))/(2*a)
+            x2 = (-b - Math.sqrt(delta))/(2*a)
+            this.x1 = !Number.isInteger(x1) ? Number( x1.toFixed(3)): x1
+            this.x2 = !Number.isInteger(x2) ? Number(x2.toFixed(3)): x2
+            x1 = this.DecimalParafracao(x1)
+            this.x1Fracao = !Number.isInteger(x1) ? x1.total: x1 
+            x2 = this.DecimalParafracao(x2)
+            this.x2Fracao = !Number.isInteger(x2) ? x2.total: x2 
+            if(!Number.isInteger(Math.sqrt(delta))){
+                let resp
+                x1 = {parte1: 0, parte2: '', total: ''} 
+                x2 = {parte1: 0, parte2: '', total: ''}
+                resp = this.DecimalParafracao(-b/(2*a))
+                x1.parte1 = Number.isInteger(resp) ? resp: resp.total
+                x2.parte1 = Number.isInteger(resp) ? resp: resp.total
+                resp = this.simplificaRaiz(delta)
+                if(typeof resp == 'number'){
+                    x1.parte2 = `√${resp}/${2*a}`
+                    x2.parte2 = `√${resp}/${2*a}`
+                    x1.total = `${x1.parte1} + ${x1.parte2}`
+                    x2.total = `${x2.parte1} - ${x2.parte2}`
+                } else {
+                    let resp2 = this.DecimalParafracao(resp.frente/(2*a))
+                    if(resp2.cima == 1){
+                        resp2.cima = ''
+                    }
+                    x1.parte2 = resp2 != 1 ? `${resp2.cima}√${resp.sobra}/${resp2.baixo}`: `√${resp.sobra}`
+                    x2.parte2 = resp2 != 1 ? `${resp2.cima}√${resp.sobra}/${resp2.baixo}`: `√${resp.sobra}`
+                    x1.total = `${x1.parte1} + ${x1.parte2}`
+                    x2.total = `${x2.parte1} - ${x2.parte2}`
+                }
+                this.x1Raiz = x1.total
+                this.x2Raiz = x2.total
+            }
+        } else {
+            let resp
+            x1 = {parte1: 0, parte2: '', total: ''}
+            x2 = {parte1: 0, parte2: '', total: ''}
+            delta *= -1
+            resp = this.DecimalParafracao(-b/(2*a))
+            x1.parte1 = Number.isInteger(resp) ? resp: resp.total
+            resp = this.DecimalParafracao(-b/(2*a))
+            x2.parte1 = Number.isInteger(resp) ? resp: resp.total
+            resp = this.DecimalParafracao(Math.sqrt(delta)/(2*a))
+            x1.parte2 = Number.isInteger(resp) ? `${resp}i`: `${resp.total}i`
+            x2.parte2 = Number.isInteger(resp) ? `${resp}i`: `${resp.total}i`
+            x1.total = `${x1.parte1} + ${x1.parte2 == '1i' ? 'i': x1.parte2}`
+            x2.total = `${x1.parte1} - ${x2.parte2 == '1i' ? 'i': x2.parte2}`
+            this.x1 = x1.total
+            this.x2 = x2.total
+            resp = this.simplificaRaiz(delta)
+            if(typeof resp == 'number'){
+                x1.parte2 = `√${resp}/${2*a}`
+                x2.parte2 = `√${resp}/${2*a}`
+                x1.total = `${x1.parte1} + ${x1.parte2}i`
+                x2.total = `${x2.parte1} - ${x2.parte2}i`
+            } else {
+                let resp2 = this.DecimalParafracao(resp.frente/(2*a))
+                if(resp2.cima == 1){
+                    resp2.cima = ''
+                }
+                x1.parte2 = resp2 != 1 ? `${resp2.cima}√${resp.sobra}i/${resp2.baixo}`: `√${resp.sobra}i`
+                x2.parte2 = resp2 != 1 ? `${resp2.cima}√${resp.sobra}i/${resp2.baixo}`: `√${resp.sobra}i`
+                x1.total = `${x1.parte1} + ${x1.parte2}`
+                x2.total = `${x2.parte1} - ${x2.parte2}`
+            }
+            this.x1Raiz = x1.total
+            this.x2Raiz = x2.total
+        }
+    }
+    DecimalParafracao(x){
+        if(!Number.isInteger(x)){
+            let neg = false
+            if(x < 0){
+                neg = true
+                x *= -1
+            }
+            let parteCima
+            let parteBaixo
+            if(x.toPrecision().length > 4){
+                x = x.toFixed(3)
+                parteCima = x*1000
+                parteBaixo = 1000
+            } else {
+                x = x.toFixed(2)
+                parteCima = x*100
+                parteBaixo = 100
+            } 
+            parteCima = Math.round(parteCima)
+            let total = {cima: parteCima, baixo: parteBaixo, total: `${parteCima}/${parteBaixo}`}
+            for(let i = 2; i <= x*100; i++){
+                do{
+                    if(Number.isInteger(parteCima/i) && Number.isInteger(parteBaixo/i)){
+                        parteCima /= i
+                        parteBaixo /= i
+                    }
+                } while(Number.isInteger(parteCima/i) && Number.isInteger(parteBaixo/i))
+                if(this.isPrimo(parteCima) || this.isPrimo(parteBaixo)){
+                    break
+                }
+            }
+            if(neg){
+                total.cima = parteCima * -1
+            } else {
+                total.cima = parteCima
+            }
+            total.baixo = parteBaixo
+            total.total = `${total.cima}/${parteBaixo}`
+            return total
+        } else {
+            return x
+        }
+    }
+    simplificaRaiz(raiz){
+        const limite = raiz
+        let total = {frente: 1, sobra: 1}
+        let possivel = false 
+        let cont = [] 
+        if(!Number.isInteger(Math.sqrt(raiz))){
+            for(let i = 0; i <= limite; i++){
+                let teste = function(){
+                    let resp
+                    cont.forEach( e =>{
+                        if(e.numero != i){
+                            resp = true 
+                        } else {
+                            resp = false 
+                        }
+                    })
+                    return resp
+                }
+                if(raiz == 1){
+                    break
+                }
+                do{
+                    if(this.isPrimo(i) && Number.isInteger(raiz/i)){
+                        if(cont.length == 0 || teste() == true){
+                            cont.push({numero: i, vezes: 1})
+                            raiz /= i
+                        } else {
+                            cont.forEach( e =>{
+                                if(e.numero == i){
+                                    e.vezes += 1
+                                    raiz /= i
+                                    if(e.vezes % 2 == 0){
+                                        total.frente *= e.numero
+                                        possivel = true
+                                    }
+                                }
+                            })
+                        }
+                    }
+                } while(Number.isInteger(raiz/i) && i >= 2)
+            }
+            if(!possivel){
+                return limite
+            } else {
+                cont.forEach( e =>{
+                    if(e.vezes % 2 == 1){
+                        total.sobra *= e.numero
+                    }
+                })
+                return total
+            }
+        } else if(!this.isPrimo(raiz)){
+            return Math.sqrt(raiz)
+        } else {
+            return raiz
+        }
+    }
+    isPrimo(x){
+        if(x == 0)
+            return false 
+        let cont = 0
+        for(let i = 0; i <= x; i++){
+            if(Number.isInteger(x/i))
+                cont++
+        }
+        if(cont == 2)
+            return true 
+        else 
+            return false 
+    }
+    xv(){
+        let a = this.a
+        let b = this.b
+        let xv = -b/(2*a)
+        this.Xv = !Number.isInteger(xv) ? Number(xv.toFixed(2)): xv  
+    }
+    yv(){
+        let a = this.a
+        let delta = this.delta
+        let yv = -delta/(4*a) == 0 ? delta/(4*a): -delta/(4*a)
+        this.Yv = !Number.isInteger(yv) ? Number(yv.toFixed(2)): yv
+    }
+} 
+// class Grafico{ sem tempo e paciencia para fazer, pois a resolução do grafico é ruim
+//     x1
+//     x2
+//     Xv
+//     Yv
+//     delta 
+//     canvas
+//     ctx
+//     altura
+//     constructor(x1, x2, xv, yv, delta, canvas, altura){
+//         this.x1 = x1
+//         this.x2 = x2
+//         this.Xv = xv
+//         this.Yv = yv
+//         this.delta = delta
+//         this.canvas = document.querySelector(canvas)
+//         this.altura = altura
+//         this.ctx = this.canvas.getContext('2d')
+//         this.grade()
+//     }
+//     grade(){
+        
+//     }
+// }
---
+++ js/script.js
@@ -0,0 +1,256 @@
+function calcular() {
+    const a = Number(document.querySelector('#a').value)
+    const b = Number(document.querySelector('#b').value)
+    const c = Number(document.querySelector('#c').value)
+    if (a == '' || b == '' || c == '') {
+        $('#dadosError').show()
+        return
+    }
+    const div = document.querySelector('.container-fluid')
+    const body = document.querySelector('#divSaida')
+    const canvas = document.createElement('canvas')
+    let escreva = document.createElement('div')
+    if (document.querySelector('#saida') != null) {
+        const remover = div.removeChild(document.querySelector('#saida'))
+    }
+    if (document.querySelector('canvas') != null) {
+        const remover = body.removeChild(document.querySelector('canvas'))
+    }
+    const formula = `${a == 0 ? '' : a == 1 ? 'x²' : a == -1 ? '-x²' : `${a}x²`} ${b != 0 ? b > 0 ? '+' : '' : ''} ${b == 0 ? '' : b == 1 ? 'x' : a == -1 ? '-x' : `${b}x`} ${c != 0 ? c > 0 ? '+' : '' : ''} ${c == 0 ? '' : c} = 0`
+    const equacao = new Equacao(a, b, c)
+    escreva.setAttribute('id', 'saida')
+    escreva.innerHTML = `<h5 class="text-center">Resultado<h5>`
+    escreva.innerHTML += `Equação: <strong>${formula}</strong><br>`
+    escreva.innerHTML += `Valor de X<small>1</small>: <strong>${equacao.x1}</strong><br>`
+    escreva.innerHTML += `Valor de X<small>2</small>: <strong>${equacao.x2}</strong><br>`
+    escreva.innerHTML += `Delta: <strong>${equacao.delta}</strong><br>`
+    if (typeof equacao.x1Fracao != 'undefined' && !Number.isInteger(equacao.x1Fracao)) {
+        escreva.innerHTML += `Fração de X<small>1</small>: <strong>${equacao.x1Fracao}</strong><br>`
+    }
+    if (typeof equacao.x2Fracao != 'undefined' && !Number.isInteger(equacao.x2Fracao)) {
+        escreva.innerHTML += `Fração de X<small>2</small>: <strong>${equacao.x2Fracao}</strong><br>`
+    }
+    if (typeof equacao.x1Raiz != 'undefined' && !Number.isInteger(equacao.x1Raiz)) {
+        escreva.innerHTML += `Raiz simplificada de X<small>1</small>: <strong>${equacao.x1Raiz}</strong><br>`
+    }
+    if (typeof equacao.x2Raiz != 'undefined' && !Number.isInteger(equacao.x2Fracao)) {
+        escreva.innerHTML += `Raiz simplificada de X<small>2</small>: <strong>${equacao.x2Raiz}</strong><br>`
+    }
+    escreva.innerHTML += `<h6 class="text-center">Para Uso do Gráfico</h6>`
+    escreva.innerHTML += `X do vértice: <strong>${equacao.Xv}</strong><br>`
+    escreva.innerHTML += `Y do vértice: <strong>${equacao.Yv}</strong><br>`
+    div.appendChild(escreva)
+    //body.appendChild(canvas)
+    document.querySelector('.rodapeFixo').classList.remove('rodapeFixo')
+    //grafico = new Grafico(equacao.x1, equacao.x2, equacao.Xv, equacao.Yv, equacao.delta, 'canvas', 500)
+}
+function registro() {
+    if (document.querySelector('#nome').value == '' || document.querySelector('#email').value == '' || document.querySelector('#senha').value == '' || document.querySelector('#confirm').value == '') {
+        $('#dadosError').show()
+        $('#meumodal').modal('toggle')
+        return
+    }
+    let senha
+    let confirmacao
+    let email
+    let confirm = false
+    let user = []
+    let nome = document.querySelector('#nome').value
+    if (!localStorage.login || !localStorage.user) {
+        localStorage.login = JSON.stringify(false)
+        localStorage.link = 'index.html'
+        localStorage.user = JSON.stringify([{ nome: 'admin', email: 'admin@gmail.com', senha: '123', logado: false, ultimaPag: 'index.html'}])
+    } else {
+        user = JSON.parse(localStorage.user)
+    }
+    document.querySelector('#senha').type = 'text'
+    document.querySelector('#confirm').type = 'text'
+    senha = document.querySelector('#senha').value
+    confirmacao = document.querySelector('#confirm').value
+    document.querySelector('#senha').type = 'password'
+    document.querySelector('#confirm').type = 'password'
+    email = document.querySelector('#email').value
+    for (let i in user) {
+        if (email == user[i].email) {
+            confirm = false
+            $('#emailError').show()
+            $('#meumodal').modal('toggle')
+            break
+        } else {
+            confirm = true
+        }
+    }
+    if (confirm) {
+        if (senha != confirmacao) {
+            confirm = false
+            $('#senhaError').show()
+            $('#meumodal').modal('toggle')
+        }
+    }
+    if (confirm) {
+        document.querySelector('#nome').value = ''
+        document.querySelector('#email').value = ''
+        document.querySelector('#senha').value = ''
+        document.querySelector('#confirm').value = ''
+        user.push({ nome: nome, email: email, senha: confirmacao, logado: true, ultimaPag: 'index.html'})
+        localStorage.user = JSON.stringify(user)
+        localStorage.login = JSON.stringify(true)
+        location.href = localStorage.link
+    }
+}
+function login() {
+    if (!localStorage.login) {
+        localStorage.login = JSON.stringify(false)
+        localStorage.link = 'index.html'
+        localStorage.user = JSON.stringify([{ nome: 'admin', email: 'admin@gmail.com', senha: '123', logado: false, ultimaPag: 'index.html' }])
+    }
+    if (document.querySelector('#emailLogin').value == '' || document.querySelector('#senhaLogin').value == '') {
+        $('#dadosError').show()
+        return
+    }
+    document.querySelector('#senhaLogin').type = 'text'
+    let senha = document.querySelector('#senhaLogin').value
+    document.querySelector('#senhaLogin').type = 'password'
+    let email = document.querySelector('#emailLogin').value
+    let LSuser = JSON.parse(localStorage.user)
+    let confirm = false
+    for (let i in LSuser) {
+        if (LSuser[i].senha == senha && LSuser[i].email == email) {
+            localStorage.login = JSON.stringify(true)
+            LSuser[i].logado = true
+            localStorage.user = JSON.stringify(LSuser)
+            document.querySelector('#emailLogin').value = ''
+            confirm = true
+            location.href = localStorage.link
+            break
+        } else {
+            confirm = false
+        }
+    }
+    if (!confirm) {
+        localStorage.login = JSON.stringify(false)
+        $('#dadosError').show()
+    }
+}
+function raizTotal() {
+    const div = document.querySelector('.container-fluid')
+    if (document.querySelector('#saida') != null) {
+        const remover = div.removeChild(document.querySelector('#saida'))
+    }
+    let valor = document.querySelector('#raiz').value
+    if (valor == '') {
+        $('#dadosError').show()
+        return
+    }
+    let neg = false
+    if (valor < 0) {
+        neg = true
+        valor *= -1
+    }
+    const raiz = new Equacao(1, 1, 1)
+    let escreva = document.createElement('div')
+    escreva.setAttribute('id', 'saida')
+    let resp = raiz.simplificaRaiz(valor)
+    if (!Number.isInteger(Number(resp))) {
+        if (neg) {
+            resp = `${resp.frente}√${resp.sobra}i`
+        } else {
+            resp = `${resp.frente}√${resp.sobra}`
+        }
+    } else {
+        if (neg) {
+            resp += 'i'
+        }
+    }
+    escreva.innerHTML = `<h5 class="text-center">Resultado<h5>`
+    escreva.innerHTML += `Valor da radicando: <strong>${neg ? valor *= -1 : valor}</strong><br>`
+    escreva.innerHTML += `Valor da raiz: <strong>${resp}</strong>`
+    div.appendChild(escreva)
+    document.querySelector('#raiz').value = ''
+}
+function decimal() {
+    const div = document.querySelector('.container-fluid')
+    if (document.querySelector('#saida') != null) {
+        const remover = div.removeChild(document.querySelector('#saida'))
+    }
+    const valor = document.querySelector('#num').value
+    if (valor == '') {
+        $('#dadosError').show()
+        return
+    }
+    const decimal = new Equacao(1, 1, 1)
+    let escreva = document.createElement('div')
+    escreva.setAttribute('id', 'saida')
+    let resp = decimal.DecimalParafracao(Number(valor))
+    if (!Number.isInteger(Number(valor))) {
+        resp = resp.total
+    }
+    escreva.innerHTML = `<h5 class="text-center">Resultado<h5>`
+    escreva.innerHTML += `Valor do número: <strong>${valor}</strong><br>`
+    escreva.innerHTML += `Valor em fração: <strong>${resp}</strong>`
+    div.appendChild(escreva)
+    document.querySelector('#num').value = ''
+}
+addEventListener('load', () => {
+    const link = location.href.split('#').join('').split('/')
+    if (localStorage.login == 'true' && link.indexOf('login.html') == -1) {
+        document.querySelector('#login').innerHTML = `<button class="btn btn-outline-success my-2 my-sm-0" onclick="javascript:window.location.href = 'index.html'; localStorage.login = false; localStorage.link = 'index.html'; let a = JSON.parse(localStorage.user); a.forEach( (e, i) =>{if(e.logado == true){a[i].logado = false; localStorage.user = JSON.stringify(a)}});">Sair</button>`
+        document.querySelector('#rodapeSair').innerHTML = `<a href='#' class="text-white" onclick="javascript:window.location.href = 'index.html'; localStorage.login = false; localStorage.link = 'index.html'; let a = JSON.parse(localStorage.user); a.forEach( (e, i) =>{if(e.logado == true){a[i].logado = false; localStorage.user = JSON.stringify(a)}});">Sair</a>`
+        if (link.indexOf('index.html') != -1) {
+            let user = JSON.parse(localStorage.user)
+            let nome
+            let ultimaPag
+            user.forEach(e => {
+                if (e.logado == true) {
+                    nome = e.nome
+                    ultimaPag = e.ultimaPag
+                }
+            })
+            document.querySelector('#nomeTitulo').innerHTML = `Bem Vindo, ${nome}!`
+            if (ultimaPag != 'index.html') {
+                let p = document.createElement('p')
+                p.innerHTML = `Deseja voltar para a última página visitada? clique <a href="${ultimaPag}" style="color: green;">aqui</a>`
+                let pIrmao = document.querySelector('#pIrmao')
+                let pai = pIrmao.parentNode
+                pai.insertBefore(p, pIrmao)
+            }
+        }
+    }
+    if (localStorage.login == 'true' && link.indexOf('login.html') != -1) {
+        let user = JSON.parse(localStorage.user)
+        user.forEach((e, i) => {
+            if (e.logado == true) {
+                user[i].logado = false
+            }
+        })
+        localStorage.user = JSON.stringify(user)
+        localStorage.login = false
+    }
+    if (localStorage.login == 'false' && link.indexOf('login.html') != -1 || !localStorage.getItem('login')) {
+        if (link.indexOf('index.html') == -1 && link.indexOf('login.html') == -1) {
+            location.href = 'login.html'
+        }
+    }
+})
+function verificacao(link2 = 'index.html') {
+    const link = location.href.split('/')
+    if (localStorage.login != 'true') {
+        if (link.indexOf('index.html') == -1 && link.indexOf('login.html') == -1) {
+            localStorage.link = link2
+            location.href = 'login.html'
+        }
+    } else {
+        let user = JSON.parse(localStorage.user)
+        if (link.indexOf(link2) == -1) {
+            if (link2 != 'index.html') {
+                user.forEach((e, i) => {
+                    if (e.logado == true) {
+                        user[i].ultimaPag = link2
+                    }
+                })
+            }
+            localStorage.user = JSON.stringify(user)
+            location.href = link2
+        }
+    }
+}
